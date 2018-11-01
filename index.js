3bd00bdb1e1c65ffc1d49405362c70f27baee072
ampersandre <mougeot.andre@gmail.com>
2018-11-01 17:38:00 -0600

    Script

---
+++ index.js
@@ -0,0 +1,55 @@
+const moment = require('moment');
+const Papa = require('papaparse');
+const data = require('./discord-data.json');
+// import * as moment from 'moment';
+// import Papa from 'papaparse';
+// import data from './discord-data.json';
+
+const users = data.meta.users;
+const usersByIndex = {};
+const messagesByUsersByDay = {};
+const dates = {};
+
+data.meta.userindex.forEach((uid, i) => {
+    usersByIndex[i] = users[uid].name;
+});
+Object.keys(usersByIndex).forEach((index) => {
+    const username = usersByIndex[index];
+    messagesByUsersByDay[username] = {};
+});
+
+const discordKey = Object.keys(data.data)[0];
+const messageData = data.data[discordKey];
+
+Object.keys(messageData).forEach((messageKey) => {
+const message = messageData[messageKey];
+const date = moment(message.t).format('YYYY-MM-DD');
+const user = usersByIndex[message.u];
+
+dates[date] = true;
+if (!messagesByUsersByDay[user][date]) {
+    messagesByUsersByDay[user][date] = 0;
+}
+
+messagesByUsersByDay[user][date] = messagesByUsersByDay[user][date] + 1;
+});
+
+const sortedDates = Object.keys(dates).sort((a, b) => {
+if(a < b) { return -1; }
+if(a > b) { return 1; }
+return 0;
+});
+
+const csvData = Object.keys(messagesByUsersByDay).map(username => {
+const row = {
+    name: username,
+};
+sortedDates.forEach(date => {
+    row[date] = messagesByUsersByDay[username][date] || 0;
+});
+
+return row;
+});
+var csv = Papa.unparse(csvData);
+
+console.log(csv);
